542349f15f40bfecd7c16b1d4067c085676719b8
sunshineyanghui <1187073986@qq.com>
2016-12-26 16:13:51 +0800

    add two components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import PostBody from './PostBody';
+import CommentBox from './CommentBox';
 class App extends React.Component {
   render () {
 
     return(
     <div>
+      <PostBody/>
+      <CommentBox/>
       hello
     </div>
     )
---
+++ src/components/CommentBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CommentBox extends React.Component{
+  render(){
+    return(
+      <div>
+        CommentBox
+      </div>
+    )
+  }
+ }
+
+export default CommentBox;
