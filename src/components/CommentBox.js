6825285521862696f6d9d9290fdc44843b9c49eb
sunshineyanghui <1187073986@qq.com>
2016-12-26 16:21:50 +0800

    readme

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 class CommentBox extends React.Component{
   render(){
     return(
-      <div>
+      <div className="commentbox">
         CommentBox
       </div>
     )
--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -3,7 +3,7 @@ import React from 'react';
 class PostBody extends React.Component{
   render(){
     return(
-      <div>
+      <div className="postbody">
         PostBody
       </div>
     )
