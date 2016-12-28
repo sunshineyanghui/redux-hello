c847def7da1688275e3f237e3ed832c53f3df921
sunshineyanghui <1187073986@qq.com>
2016-12-28 09:01:35 +0800

    num

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import store from '../store';
 class PostBody extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      num: store.getState().length
+    }
+  }
   render(){
     return(
       <div className="post-body">
         <div className="comment-num">
-          24
+          { this.state.num }
         </div>
       </div>
     )
