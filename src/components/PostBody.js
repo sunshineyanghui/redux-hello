5ba32dc9cfeff6bf2dddd21c1f0da4b6052e2b62
sunshineyanghui <1187073986@qq.com>
2016-12-30 10:54:15 +0800

    likes

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import store from '../store';
 class PostBody extends React.Component{
   constructor(){
     super();
+  }
+  handleClick(id){
+    store.dispatch({type:'INCREMENT_LIKES',index:id-1})
   }
   render(){
     return(
       <div className="post-body">
         {this.props.id}
+        <div className="comment-likes" onClick={this.handleClick.bind(this,this.props.id)}>
+          {this.props.posts[this.props.id-1].likes}喜欢
+        </div>
         <div className="comment-num">
-          { this.props.comments[this.props.id].length }
+          { this.props.comments[this.props.id].length }评论
         </div>
       </div>
     )
   }
  }
 const mapStateToProps = (state) =>({
-  comments:state.comments
+  comments:state.comments,
+  posts:state.posts
 });
 export default connect (mapStateToProps)(PostBody);
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,17 @@
 function postReducer(state = [], action) {
-  return state
+  console.log(state)
+  console.log('----------------------');
+  console.log(action);
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1),
+      ]
+    default:
+      return state;
+  }
 }
 export default postReducer;
